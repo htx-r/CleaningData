797abb126391f8ecd5323c771f02a284b4b682e6
Tasnim <tasneemhamza1989@gmail.com>
2019-06-11 15:33:09 +0200

    updated function

--- R/cleanBIOGENtrialsfun.R
+++ R/cleanBIOGENtrialsfun.R
@@ -1,110 +1,86 @@
+##########################################################################################
+
+# 1. I chose the 'objective relpase assessment' (PARAMCD='OBJREL') as a type of the relapse
+# However, it is the same as 'all realpse assessment'.
+# 2. Create a dummy varaible from the AVAL varaible for the 1 year relapses (AVISIT='0-1 Year')
+# 3. Create a dummy varaible from the AVAL varaible for the 2 years relapses (AVISIT='Overall 0-2 Years')
+# 4. Check if the calculated dummy variables are correct.
+# 5. Check if the IPD compatible with AD (form MS file)
+
+# PS: AVAL counts the number of the relapses to each type of the assessment in PARAMCD.
+
+#########################################################################################
+
+datapath="/Users/tasneem/Desktop/TasnimPhD/multiple sclerosis/IPD data from 6 Biogen trials"
+
 cleanBIOGENtrials.fun=function(datapath){
-
-library(readxl)
-
-
+  rm(list=ls())
+  
+  library(readxl)
+  
+  ## Read the IPD biogen data
+  
+  # path
   adslpath=paste(datapath,"/adsl.csv",sep="")
   adarrpath=paste(datapath,"/adarr.csv",sep="")
   adrelpath=paste(datapath,"/adrel.csv",sep="")
-
-
-## Read Biogen dataset that contains the demographic and baseline information
-# remove biogen from the name dataset.
-adsl <- read.csv(adslpath)
-adarr <- read.csv(adarrpath)
-adrel <- read.csv(adrelpath)
-
-## Task1: Add two columns(0/1) RELAPSE1year and RELAPSE2year to ADSL dataset
-nsubjects <- length(unique(adarr$USUBJID))
-usubjectID <- unique(adarr$USUBJID)
-
-## Consider the 'all relapse assessment' method to diagnose MS
-adarr_ALLREL <- adarr[adarr$PARAMCD=='OBJREL',]
-
-## transfer the RELAPSE within 1 year to 0/1
-adarr_ALLREL01year <- adarr_ALLREL[adarr_ALLREL$AVISIT=='0-1 Year',]
-RELAPSE01Year <-  ifelse(adarr_ALLREL01year$SUBJRR>0,1,0)
-length(RELAPSE01Year) ## all individuals have 1 year assessment
-
-## transfer the RELAPSE within 2 years to 0/1: 2 steps
-
-
-# Not all the patients have been checked within overall 0-2 Years
-# 1. I extract the row for each subject who has 'Overall 0-2 Years' assessment (adarr_ALLREL_02year 'list of dataframes') then
-# 2. I check the nrow() of each data frame, if nrow==0 then there is no assessment for 'Overall 0-2 Years'
-   # If not (nrow==1), then I check if the patient relapsed or not ('SUBJRR' differ from zero or not).
-
-# 1.
-adarr_ALLREL_02year <- sapply(1:nsubjects, function(i) subset(adarr_ALLREL,subset = adarr_ALLREL$USUBJID == as.character(usubjectID[i]) & adarr_ALLREL$AVISIT=='Overall 0-2 Years'),
-             simplify = F)
-
-
-# 2.
-RELAPSE02Year <- 1:nsubjects
-for (i in 1:nsubjects) {
-
-  if(nrow(adarr_ALLREL_02year[[i]]) == 0){
-    RELAPSE02Year[i] <- NA
-  }else{
-    if(adarr_ALLREL_02year[[i]]$SUBJRR>0){
-      RELAPSE02Year[i] <- 1
-
+  
+  # data
+  adsl <- read.csv(adslpath)
+  adarr <- read.csv(adarrpath)
+  adrel <- read.csv(adrelpath)
+  
+  ## 1. Create the dataset with only the 'objective relpase assessment' (PARAMCD='OBJREL')
+  adarr_OBJREL <- adarr[adarr$PARAMCD=='OBJREL',]
+  
+  
+  ## 2. Create a dummy varaible (from AVAL) for the within 1 year relapses RELAPSE01Year
+  adarr_OBJREL01year <- adarr_OBJREL[adarr_OBJREL$AVISIT=='0-1 Year',]
+  RELAPSE01Year <-  ifelse(adarr_OBJREL01year$AVAL>0,1,0)
+  
+  ## 3. Create a dummy varaible (from AVAL) for the within 2 years relapses RELAPSE02Year
+  
+  #%%% All the patients have been checked within overall 0-2 Years except one study (only 0-1 year).
+  # so it is not straight forward as 0-1 year where all studies have data.
+  
+  # 3.1 I extract the row for each subject who has 'Overall 0-2 Years' assessment (adarr_OBJREL_02year 'list of dataframes') then
+  nsubjects <- length(unique(adarr$USUBJID))
+  usubjectID <- unique(adarr$USUBJID)
+  
+  adarr_OBJREL_02year_ds <- sapply(1:nsubjects, function(i) subset(adarr_OBJREL,subset = adarr_OBJREL$USUBJID == as.character(usubjectID[i]) & adarr_OBJREL$AVISIT=='Overall 0-2 Years'),
+                                   simplify = F)
+  
+  # 3.2 I check the nrow() of each data frame, if nrow==0 then there is no assessment for 'Overall 0-2 Years'
+  # If not (nrow==1), then I check if the patient relapsed or not ('SUBJRR' differ from zero or not).
+  
+  RELAPSE02Year <- 1:nsubjects
+  for (i in 1:nsubjects) {
+    
+    if(nrow(adarr_OBJREL_02year_ds[[i]]) == 0){
+      RELAPSE02Year[i] <- NA
     }else{
-      RELAPSE02Year[i] <- 0
-
+      if(adarr_OBJREL_02year_ds[[i]]$AVAL>0){
+        RELAPSE02Year[i] <- 1
+        
+      }else{
+        RELAPSE02Year[i] <- 0
+        
+      }
     }
+    
   }
-
-}
-
-## Add the two columns to ADSL dataset
-adsl01 <- adsl
-adsl01$RELAPSE1year <- RELAPSE01Year
-adsl01$RELAPSE2year <- RELAPSE02Year
-
-### Check:
-# Table for each study how many patients have and have not relapsed
-
-# within 2 years
-m <-as.matrix(table(adsl01$STUDYID,adsl01$RELAPSE2year))
-mm <- apply(m, 1, sum)
-mm
-
-# within one year
-m1 <-as.matrix(table(adsl01$STUDYID,adsl01$RELAPSE1year))
-mm1 <- apply(m1, 1, sum)
-mm1
-
-# Table: for each study, the total number of relpases over time
-table(adarr_ALLREL$AVISIT,adarr_ALLREL$STUDYID)
-
-# Notice that the values in mm equals the last line in the last table
-
-# Overall 0-2 Years        0     1234     1417    939   1171     301
-
-# While the values in mm1 equals the first line in the last table
-
-#  0-1 Year              1512     1234     1417    939   1171     301
-
-#%%%% I think now everything is correct
-
-## Task2: Correct drug names/study names to be matched with drug names/study names in MS dataset
-   #%%%%% change the levels of adsl$TRT01A to MSdataset
-## read MS aggregated dataset
-MSdataset <- read_excel('MSaggregated.xlsx')
-MSdataset <- as.data.frame(MSdataset)
-names(table(MSdataset$treat))
-
-levels(adsl01$TRT01A )
-
-## Change the names (levels) of the drugs in ADSL dataset
-levels(adsl01$TRT01A )[c(2,3,6)] <- c('Dimethyl fumarate','Dimethyl fumarate','Glatiramer acetate')
-
-## Change the names (levels) of the studies names in ADSL dataset
-
-levels(adsl01$STUDYID) <- c('ADVANCE', 'DEFINE', 'CONFIRM','AFFIRM','SENTINEL','MSCRG')
-#
-
-
-####!!!!!!!! remove what data is no longer needed
+  
+  ## 3.3 Add the two dummy variables (RELAPSE01Year and RELAPSE02Year) to ADSL dataset now it is adsl01
+  adsl01 <- adsl
+  adsl01$RELAPSE1year <- RELAPSE01Year
+  adsl01$RELAPSE2year <- RELAPSE02Year
+  
+  # change names of the studies
+  levels(adsl01$STUDYID) <- c('ADVANCE', 'DEFINE', 'CONFIRM','AFFIRM','SENTINEL','MSCRG')
+  
+  # change the names (levels) of the drugs in ADSL dataset
+  levels(adsl01$TRT01A )[c(2,3,6)] <- c('Dimethyl fumarate','Dimethyl fumarate','Glatiramer acetate')
+  
+  # return both datastes
+  return(list(adsl01=adsl01,adarr_OBJREL=adarr_OBJREL))
 }
